a786e22af5c54b369ed353d15aa61bc6fe9bbac9
ab7z <abhiksi613@gmail.com>
2021-08-11 12:54:27 +0530

    last commit

--- server.js
+++ server.js
@@ -129,5 +129,5 @@ io.on("connection", socket => {
         }
     })
 })
-
-server.listen(3000, () => console.log("server started on port 3000"));
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log("server started on port " + PORT));
